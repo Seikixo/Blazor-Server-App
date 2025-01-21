b255d9e8d7ba7883574346c3c7359e70b4b683a3
Vaughn Fitz Benedicto <vaughnfitz27@gmail.com>
2025-01-21 17:43:19 +0800

    Initial Commit

---
+++ Pages/Storage/PersonDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAppJS.Pages.Storage
+{
+    public class PersonDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ wwwroot/storage.js
@@ -0,0 +1,26 @@
+﻿var objectStorage = [];
+var dotnetRef = null; // the dotnet referenec
+
+
+export function initialize(dotnetReference) {
+    dotnetRef = dotnetReference;
+}
+
+export function insertObject(obj) {
+    objectStorage.push(obj);
+    // update the c# component
+    dotnetRef.invokeMethodAsync("InsertToCSharpList", obj);
+}
+
+export function removeObject(obj) {
+    var existingObject = objectStorage.find(x => x.Id === obj.Id);
+
+    if (existingObject) {
+        var index = objectStorage.indexOf(existingObject);
+        if (index !== -1) {
+            objectStorage.splice(index, 1);
+            dotnetRef.invokeMethodAsync("RemoveFromCSharpList", existingObject);
+        }
+    }
+}
+
