404105fd9060071b17f68720365e78e2420ce599
Vaughn Fitz Benedicto <vaughnfitz27@gmail.com>
2025-01-22 09:22:41 +0800

    Added Comments

--- wwwroot/storage.js
+++ wwwroot/storage.js
@@ -1,25 +1,25 @@
-﻿var objectStorage = [];
-var dotnetRef = null; // the dotnet referenec
+﻿var objectStorage = []; // The storage for the objects
+var dotnetRef = null; // The dotnet reference to call C# methods
 
 
-export function initialize(dotnetReference) {
+export function initialize(dotnetReference) { //Saves the dotnetRef to enable future calls to C# methods.
     dotnetRef = dotnetReference;
 }
 
 export function insertObject(obj) {
-    objectStorage.push(obj);
-    // update the c# component
-    dotnetRef.invokeMethodAsync("InsertToCSharpList", obj);
+    objectStorage.push(obj); //Adds the person object (obj) to the objectStorage array.
+    
+    dotnetRef.invokeMethodAsync("InsertToCSharpList", obj); //Invoke the C# InsertToCSharpList method to update the Blazor list.
 }
 
 export function removeObject(obj) {
-    var existingObject = objectStorage.find(x => x.Id === obj.Id);
+    var existingObject = objectStorage.find(x => x.Id === obj.Id); //Finds objects in objectStorage matching the id of the provided obj.
 
     if (existingObject) {
         var index = objectStorage.indexOf(existingObject);
         if (index !== -1) {
             objectStorage.splice(index, 1);
-            dotnetRef.invokeMethodAsync("RemoveFromCSharpList", existingObject);
+            dotnetRef.invokeMethodAsync("RemoveFromCSharpList", existingObject); //Invoke the C# RemoveFromCSharpList method to update the Blazor list.
         }
     }
 }
